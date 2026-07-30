62b9a334c59658580c696f05a3af8b8b5726ba24
0justdoit1 <0justdoit1@gmail.com>
2026-07-30 11:30:46 +0900

    fix: preprocessing.py에서 실수로 지웠던 get_slide_thumbnail 복구

--- mosec-serving/preprocessing.py
+++ mosec-serving/preprocessing.py
@@ -45,3 +45,8 @@ def get_tissue_patch_coords(slide: openslide.OpenSlide, patch_size: int = PATCH_
 
     del thumb, thumb_gray, tissue_mask
     return coords
+
+
+def get_slide_thumbnail(slide: openslide.OpenSlide, max_size: int = 4096) -> Image.Image:
+    """결과 화면 원본 뷰용 고해상도 썸네일 (확대/팬 대응, Deep Zoom 아님)."""
+    return slide.get_thumbnail((max_size, max_size))
